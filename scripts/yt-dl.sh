3f21e4bf8e76261f1a7e7e1d1e6ad4cda8f4aa5e
dioptre <dioptre@gmail.com>
2021-03-31 14:38:23 -0700

    wip

--- scripts/yt-dl.sh
+++ scripts/yt-dl.sh
@@ -1,4 +1,5 @@
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
+youtube-dl --user-agent '' -F zLuVsFGXukc
 youtube-dl --merge-output-format mkv --user-agent '' -f 313+251 zLuVsFGXukc
 ./ffmpeg -i ../notre.mkv -vf transform360="input_stereo_format=TB" -y ../output.mp4
